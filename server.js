93e1b3bfe5f5c60265d965d77481d67bf4612acb
sigmasolutionsx-droid <sigmasolutionsx@gmail.com>
2025-12-21 16:08:50 -0500

    fix: disable strict proxy validation

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100
-  validate: { xForwardedForHeader: false }
+  validate: false
 });
 app.use('/api/', limiter);
 
